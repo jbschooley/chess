65f17d0e132f2c06bfe547e40dc724b1abe770c3
Jacob Schooley <jacob.schooley@gmail.com>
2024-01-08 21:32:45 -0700

    BishopMoveTests working

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Represents a single chess piece
@@ -52,6 +54,128 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = null;
+        switch (type) {
+            case KING:
+//                moves = kingMoves(board, myPosition);
+                break;
+            case QUEEN:
+//                moves = queenMoves(board, myPosition);
+                break;
+            case BISHOP:
+                moves = bishopMoves(board, myPosition);
+                break;
+            case KNIGHT:
+//                moves = knightMoves(board, myPosition);
+                break;
+            case ROOK:
+//                moves = rookMoves(board, myPosition);
+                break;
+            case PAWN:
+//                moves = pawnMoves(board, myPosition);
+                break;
+        }
+        return moves;
+    }
+
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new HashSet<ChessMove>();
+        for (int i = 1; i < 8; i++) {
+            ChessPosition newPos = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() + i);
+
+            ChessMove move = isValidMove(board, myPosition, newPos);
+            if (move != null) {
+                moves.add(move);
+            } else {
+                ChessMove capture = isValidCapture(board, myPosition, newPos);
+                if (capture != null) {
+                    moves.add(capture);
+                }
+                break;
+            }
+
+        }
+
+        for (int i = 1; i < 8; i++) {
+            ChessPosition newPos = new ChessPosition(myPosition.getRow() - i, myPosition.getColumn() - i);
+
+            ChessMove move = isValidMove(board, myPosition, newPos);
+            if (move != null) {
+                moves.add(move);
+            } else {
+                ChessMove capture = isValidCapture(board, myPosition, newPos);
+                if (capture != null) {
+                    moves.add(capture);
+                }
+                break;
+            }
+        }
+
+        for (int i = 1; i < 8; i++) {
+            ChessPosition newPos = new ChessPosition(myPosition.getRow() - i, myPosition.getColumn() + i);
+
+            ChessMove move = isValidMove(board, myPosition, newPos);
+            if (move != null) {
+                moves.add(move);
+            } else {
+                ChessMove capture = isValidCapture(board, myPosition, newPos);
+                if (capture != null) {
+                    moves.add(capture);
+                }
+                break;
+            }
+        }
+
+        for (int i = 1; i < 8; i++) {
+            ChessPosition newPos = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() - i);
+
+            ChessMove move = isValidMove(board, myPosition, newPos);
+            if (move != null) {
+                moves.add(move);
+            } else {
+                ChessMove capture = isValidCapture(board, myPosition, newPos);
+                if (capture != null) {
+                    moves.add(capture);
+                }
+                break;
+            }
+        }
+
+        return moves;
+    }
+
+    private ChessMove isValidMove(ChessBoard board, ChessPosition myPosition, ChessPosition newPos) {
+        if (newPos.getRow() < 1 || newPos.getRow() > 8 || newPos.getColumn() < 1 || newPos.getColumn() > 8) {
+            return null;
+        }
+
+        if (board.getPiece(newPos) == null) {
+            return new ChessMove(myPosition, newPos, null);
+        }
+
+        return null;
+    }
+
+    private ChessMove isValidCapture(ChessBoard board, ChessPosition myPosition, ChessPosition newPos) {
+        if (newPos.getRow() < 1 || newPos.getRow() > 8 || newPos.getColumn() < 1 || newPos.getColumn() > 8) {
+            return null;
+        }
+
+        if (board.getPiece(newPos) != null) {
+            if (board.getPiece(newPos).getTeamColor() != this.getTeamColor()) {
+                return new ChessMove(myPosition, newPos, null);
+            }
+        }
+        return null;
+    }
+
+    private ChessMove isValidMoveOrCapture(ChessBoard board, ChessPosition myPosition, ChessPosition newPos) {
+        ChessMove move = isValidMove(board, myPosition, newPos);
+        if (move != null) {
+            return move;
+        } else {
+            ChessMove capture = isValidCapture(board, myPosition, newPos);
+            return capture;
+        }
     }
 }
