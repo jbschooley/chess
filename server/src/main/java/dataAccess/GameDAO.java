da0099061367c52bc6c559c4b5a631fe56f1e89a
Jacob Schooley <jacob@schooley.com>
2024-02-12 15:09:28 -0700

    add DAO interface methods

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -1,4 +1,13 @@
 package dataAccess;
 
+import model.GameData;
+
+import java.util.Collection;
+
 public interface GameDAO {
+    void clear() throws DataAccessException;
+    GameData createGame(String name) throws DataAccessException;
+    GameData getGame(int id) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    GameData updateGame() throws DataAccessException; // TODO args
 }
