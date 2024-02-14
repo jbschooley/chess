334e08b606b86751055a0212b3d54f47608eb05f
Jacob Schooley <jacob@schooley.com>
2024-02-14 14:08:39 -0700

    add placeholder returns to get rid of errors

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -6,7 +6,11 @@ import model.UserData;
 record LoginResult(String username, String authToken) {}
 
 public class UserService {
-    public AuthData register(UserData user) {}
-    public AuthData login(UserData user) {}
+    public AuthData register(UserData user) {
+        return null;
+    }
+    public AuthData login(UserData user) {
+        return null;
+    }
     public void logout(UserData user) {}
 }
