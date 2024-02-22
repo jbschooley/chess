9051fb25734e4db20daaca7b89407d53aa24ccf1
Jacob Schooley <jacob@schooley.com>
2024-02-21 23:31:06 -0700

    add register handler and service, passes register tests

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,17 +1,26 @@
 package server;
 
+import com.google.gson.Gson;
 import dataAccess.*;
+import exceptions.UserAlreadyTakenException;
+import model.AuthData;
+import model.UserData;
 import service.ClearService;
+import service.UserService;
 import spark.*;
-
-import java.nio.file.Paths;
 
 public class Server {
 
+    Gson serializer = new Gson();
+
+    // DAOs
     GameDAO gameDao = new MemoryGameDAO();
     AuthDAO authDao = new MemoryAuthDAO();
     UserDAO userDao = new MemoryUserDAO();
+
+    // Services
     ClearService clearService = new ClearService(gameDao, authDao, userDao);
+    UserService userService = new UserService(authDao, userDao);
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -19,10 +28,9 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-        Spark.delete("/db", (req, res) -> {
-            clearService.clear();
-            return "";
-        });
+        Spark.delete("/db", this::clearHandler);
+
+        Spark.post("/user", this::register);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -33,7 +41,34 @@ public class Server {
         Spark.awaitStop();
     }
 
-    void ClearHandler() throws DataAccessException {
+    Object clearHandler(Request req, Response res) throws DataAccessException {
         clearService.clear();
+        return "";
+    }
+
+    Object register(Request req, Response res) throws DataAccessException {
+        UserData u = serializer.fromJson(req.body(), UserData.class);
+        if (u.username() == null || u.password() == null || u.email() == null) {
+            return badRequest(res);
+        }
+        try {
+            AuthData a = userService.register(u);
+            return serializer.toJson(a);
+        } catch (DataAccessException e) {
+            res.status(400);
+            return badRequest(res);
+        } catch (UserAlreadyTakenException e) {
+            res.status(403);
+            return errorJson("already taken");
+        }
+    }
+
+    String errorJson(String message) {
+        return "{ \"message\": \"Error: " + message + "\" }";
+    }
+
+    String badRequest(Response res) {
+        res.status(400);
+        return errorJson("bad request");
     }
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,16 +1,41 @@
 package service;
 
+import com.google.gson.Gson;
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.UserDAO;
+import exceptions.UserAlreadyTakenException;
 import model.AuthData;
 import model.UserData;
 
-record LoginResult(String username, String authToken) {}
+//record LoginResult(String username, String authToken) {}
 
 public class UserService {
-    public AuthData register(UserData user) {
-        return null;
+
+    AuthDAO authDao;
+    UserDAO userDao;
+
+
+    public UserService(AuthDAO authDao, UserDAO userDao) {
+        this.authDao = authDao;
+        this.userDao = userDao;
     }
+
+    public AuthData register(UserData user) throws DataAccessException, UserAlreadyTakenException {
+        try {
+            userDao.getUser(user.username());
+            throw new UserAlreadyTakenException();
+        } catch (DataAccessException e) {
+            // user does not exist, so create new
+            userDao.createUser(user);
+            return authDao.createAuth(user.username());
+        }
+    }
+
     public AuthData login(UserData user) {
         return null;
     }
+
     public void logout(UserData user) {}
 }
+
